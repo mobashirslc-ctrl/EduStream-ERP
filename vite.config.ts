4c5c581df58f2681aa470bf184406ffba1d3ee3c
mobashirslc-ctrl <mobashir.slc@gmail.com>
2026-04-26 13:27:21 +0600

    fix: updated config files to recognize component path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // এটি অবশ্যই যোগ করবেন
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // @ কে src ফোল্ডারের সাথে লিঙ্ক করা
+    },
+  },
 })
